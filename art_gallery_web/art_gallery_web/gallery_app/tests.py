39c1651e8bb831c4cba90bf26739d95d5555bc02
nedkoo <87270759+nedkoo@users.noreply.github.com>
2021-08-13 22:13:20 +0300

    Tests are added. Responsive pages.

--- art_gallery_web/art_gallery_web/gallery_app/tests.py
+++ art_gallery_web/art_gallery_web/gallery_app/tests.py
@@ -1,3 +1,131 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from django.test import TestCase, Client
+
+from django.urls import reverse
+
+from art_gallery_web.gallery_app.models import is_negative, Arts, Post
+
+
+# test custom validator in Arts_Model
+class ValidateIsNegativeTests(TestCase):
+    def test_whenValueIsGreaterThanZero_expectToDoNothing(self):
+        value = 5
+        is_negative(value)
+
+    def test_whenValueIsZero_expectToRaise(self):
+        value = 0
+        with self.assertRaises(ValidationError) as message:
+            is_negative(value)
+        self.assertIsNotNone(message.exception)
+
+    def test_whenValueIsLessThanZero_expectToRaise(self):
+        value = -3
+        with self.assertRaises(ValidationError) as message:
+            is_negative(value)
+        self.assertIsNotNone(message.exception)
+
+
+# test model Arts
+class ArtModelTest(TestCase):
+    def test_whenHeightWeightPriceIsZero_expectToRaise(self):
+        name = 'Test1'
+        height = 0
+        width = 0
+        price = 0
+        description = 'The test1'
+        mock_image = SimpleUploadedFile(name='test_image1.jpg', content=b'', content_type='image/jpeg')
+        user = User.objects.create(username='Test1', password='test1123456')
+        test_art = Arts(name=name, height=height, width=width, price=price, description=description, image=mock_image,
+                        created_by=user)
+
+        with self.assertRaises(ValidationError) as message:
+            test_art.full_clean()
+            test_art.save()
+        self.assertIsNotNone(message.exception)
+
+    def test_whenPriceIsLessZero_expectToRaise(self):
+        name = 'Test2'
+        height = 30
+        width = 50
+        price = -30
+        description = 'The test2'
+        mock_image = SimpleUploadedFile(name='test_image2.jpg', content=b'', content_type='image/jpeg')
+        user = User.objects.create(username='Test2', password='test2123456')
+        test_art = Arts(name=name, height=height, width=width, price=price, description=description, image=mock_image,
+                        created_by=user)
+
+        with self.assertRaises(ValidationError) as message:
+            test_art.full_clean()
+            test_art.save()
+        self.assertIsNotNone(message.exception)
+
+    def test_whenHeightWeightPriceIsPositive_expectSuccess(self):
+        name = 'Test3'
+        height = 30
+        width = 50
+        price = 130
+        description = 'The test2 '
+        mock_image = SimpleUploadedFile(name='test_image3.jpg', content=b'', content_type='image/jpeg')
+        user = User.objects.create(username='Test3', password='test2123456')
+        test_art = Arts(name=name, height=height, width=width, price=price, description=description, image=mock_image,
+                        created_by=user)
+
+        test_art.full_clean()
+        test_art.save()
+
+
+# test model Post
+class PostModelTest(TestCase):
+    # mock art
+    def test_PostModel_expectSuccess(self):
+        name = '1'
+        height = 30
+        width = 50
+        price = 130
+        description = '1'
+        mock_image = SimpleUploadedFile(name='test_image_post.jpg', content=b'', content_type='image/jpeg')
+        user = User.objects.create(username='Post', password='postpost123')
+        test_art_post = Arts(name=name, height=height, width=width, price=price, description=description,
+                             image=mock_image,
+                             created_by=user)
+        test_art_post.full_clean()
+        test_art_post.save()
+        # mock post
+        text = 'Hello'
+        name_post = 'Test_Name'
+        email = 'test@gmail.com'
+        created_on = datetime.datetime(2008, 5, 5, 16, 20)
+        test_post = Post(text=text, created_on=created_on, name=name_post, email=email, art=test_art_post)
+
+        test_post.full_clean()
+        test_post.save()
+
+
+# test indexView
+class IndexViewTests(TestCase):
+    def test_index(self):
+        client = Client()
+        response = client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'index.html')
+
+
+# test aboutView
+class AboutViewTests(TestCase):
+    def test_about(self):
+        client = Client()
+        response = client.get(reverse('about page'))
+        self.assertTemplateUsed(response, 'about.html')
+
+
+# test listView
+class ListViewTests(TestCase):
+    def test_about(self):
+        client = Client()
+        response = client.get(reverse('list arts'))
+        self.assertTemplateUsed(response, 'arts/arts_list.html')
--- art_gallery_web/art_gallery_web/gallery_auth/tests.py
+++ art_gallery_web/art_gallery_web/gallery_auth/tests.py
@@ -1,3 +1,85 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model, authenticate
+from django.test import TestCase, Client
 
-# Create your tests here.
+from django.urls import reverse
+
+from art_gallery_web.gallery_app.models import Arts
+
+
+class ProfileModelTests(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create()
+
+    def test_profile(self):
+        userprofile = get_user_model().objects.last().userprofile
+
+
+UserModel = get_user_model()
+
+
+class ArtTestCase(TestCase):
+    logged_in_user_username = 'nedkorachev'
+    logged_in_user_email = 'nedko@rachev.it'
+    logged_in_user_password = '12345qwe'
+
+    def assertListEmpty(self, ll):
+        return self.assertListEqual([], ll, 'The list is not empty')
+
+    def setUp(self):
+        self.client = Client()
+        self.user = UserModel.objects.create_user(
+            email=self.logged_in_user_email,
+            password=self.logged_in_user_password,
+            username=self.logged_in_user_username,
+        )
+
+
+class ProfileDetailsTest(ArtTestCase):
+    def test_getDetails_whenLoggedInUserWithNoArts_shouldGetDetailsWithNoArts(self):
+        self.client.force_login(self.user)
+
+        response = self.client.get(reverse('my_profile'))
+
+        self.assertListEmpty(list(response.context['arts']))
+
+    def test_getDetails_whenLoggedInUserWithArts_shouldGetDetailsWithArts(self):
+        art = Arts.objects.create(
+            name='Test Art',
+            height=10,
+            width=20,
+            price=120,
+            description='TEst art description',
+            image='path/to/image.png',
+
+            created_by=self.user,
+        )
+
+        self.client.force_login(self.user)
+
+        response = self.client.get(reverse('my_profile'))
+
+        self.assertEqual(200, response.status_code)
+
+        self.assertListEqual([art], list(response.context['arts']))
+
+
+class LoginTest(ArtTestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='test', password='test1234', email='test@example.com')
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_correct(self):
+        user = authenticate(username='test', password='test1234')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_wrong_username(self):
+        user = authenticate(username='wrong', password='test1234')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_wrong_password(self):
+        user = authenticate(username='test', password='wrong')
+        self.assertFalse(user is not None and user.is_authenticated)
